25c0e2b66ea9947b512296c2eab7aed23acbc7b2
Crowdbotics <team@crowdbotics.com>
2024-02-20 18:37:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mindfulmoney-46507",
+  url: "https://mindfulmoney-46507.botics.co",
+  api: "https://mindfulmoney-46507.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
